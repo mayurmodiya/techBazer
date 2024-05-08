ef6e390f5c47bd8ca06cbdc61e3ef0d593addbaf
shohag <shohagmiah2100@gmail.com>
2024-05-08 20:59:19 +0600

    all fiexd

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,14 @@
+import Loader from '@/components/others/Loader'
 import DashboardPageOne from '@/components/pages/dashboard-pages/DashboardPageOne'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const DashboardPage = () => {
   return (
-    <main className='w-full'>
+    <Suspense fallback={<Loader />}>
+      <main className='w-full'>
       <DashboardPageOne />
     </main>
+    </Suspense>
   )
 }
 
